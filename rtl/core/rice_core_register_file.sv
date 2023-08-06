2ce7e23f87dcb6134d045f4509f47f51d98bde51
Taichi Ishitani <taichi730@gmail.com>
2023-08-06 21:11:53 +0900

    simplify register file implementation

--- rtl/core/rice_core_register_file.sv
+++ rtl/core/rice_core_register_file.sv
@@ -6,37 +6,31 @@ module rice_core_register_file
   input var                 i_clk,
   rice_core_pipeline_if.rf  pipeline_if
 );
+  localparam  int RF_SIZE = 32;
+
   `rice_core_define_types(XLEN)
 
-  rice_core_value [31:0]  register_file;
-  rice_core_ex_result     ex_result;
+  rice_core_value     register_file[RF_SIZE];
+  rice_core_ex_result ex_result;
 
   always_comb begin
-    pipeline_if.register_file = register_file;
+    for (int i = 0;i < RF_SIZE;++i) begin
+      if (i == 0) begin
+        pipeline_if.register_file[i]  = rice_core_value'(0);
+      end
+      else begin
+        pipeline_if.register_file[i]  = register_file[i];
+      end
+    end
   end
 
   always_comb begin
     ex_result = pipeline_if.ex_result;
   end
 
-  for (genvar i = 0;i < 32;++i) begin : g_register_file
-    if (i == 0) begin : g
-      always_comb begin
-        register_file[i]  = '0;
-      end
-    end
-    else begin : g
-      logic update_rg;
-
-      always_comb begin
-        update_rg = ex_result.valid && (ex_result.rd == rice_core_rd'(i));
-      end
-
-      always_ff @(posedge i_clk) begin
-        if (update_rg) begin
-          register_file[i]  <= ex_result.rd_value;
-        end
-      end
+  always_ff @(posedge i_clk) begin
+    if (ex_result.valid && (ex_result.rd != rice_core_rd'(0))) begin
+      register_file[ex_result.rd] <= ex_result.rd_value;
     end
   end
 endmodule
