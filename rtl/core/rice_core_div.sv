519fac6dc3b9b85123ed61562a53090a90e5c97b
Taichi Ishitani <taichi730@gmail.com>
2023-08-29 11:29:30 +0900

    skip sign bit of dividend to reduce latency of DIV insturction

--- rtl/core/rice_core_div.sv
+++ rtl/core/rice_core_div.sv
@@ -12,22 +12,25 @@ module rice_core_div
   output  var                         o_result_valid,
   output  var [XLEN-1:0]              o_result
 );
-  localparam  int REMAINDER_WIDTH = 2 * XLEN;
-  localparam  int COUNT_WIDTH     = $clog2(XLEN + 2);
-  localparam  int INITIAL_COUNT   = XLEN + 1;
-  localparam  int SUB_WIDTH       = XLEN + 1;
+  localparam  int DIVIDEND_WIDTH  = XLEN + 1;
+  localparam  int RESULT_WIDTH    = 2 * XLEN;
+  localparam  int INITIAL_COUNT   = XLEN + 1; //  xlen + output
+  localparam  int COUNT_WIDTH     = $clog2(INITIAL_COUNT + 1);
+
+  localparam  bit [XLEN-1:0]  VALUE_MIN     = {1'(1), {XLEN-1{1'b0}}};
+  localparam  bit [XLEN-1:0]  VALUE_MINUS_1 = '1;
 
   typedef struct packed {
-    logic [REMAINDER_WIDTH-1:0] remainder;
-    logic [XLEN-1:0]            quotient;
-  } rice_core_division_result;
+    logic [COUNT_WIDTH-1:0]   count;
+    logic [RESULT_WIDTH-1:0]  result;
+  } rice_core_div_initial_value;
 
-  logic [COUNT_WIDTH-1:0]   count;
-  logic                     calcuration;
-  logic                     finish;
-  logic                     busy;
-  logic                     start;
-  rice_core_division_result result;
+  rice_core_div_initial_value initial_value;
+  logic [COUNT_WIDTH-1:0]     count;
+  logic                       start;
+  logic                       calcuration;
+  logic                       finish;
+  logic [RESULT_WIDTH-1:0]    result;
 
   always_comb begin
     o_result_valid  = finish;
@@ -35,10 +38,13 @@ module rice_core_div
   end
 
   always_comb begin
+    initial_value = get_initial_value(i_div_operation, i_rs1_value);
+  end
+
+  always_comb begin
+    start       = i_valid && (count == COUNT_WIDTH'(0));
     calcuration = count >= COUNT_WIDTH'(2);
     finish      = count == COUNT_WIDTH'(1);
-    busy        = calcuration || finish;
-    start       = i_valid && (!busy);
   end
 
   always_ff @(posedge i_clk, negedge i_rst_n) begin
@@ -46,16 +52,16 @@ module rice_core_div
       count <= COUNT_WIDTH'(0);
     end
     else if (start) begin
-      count <= COUNT_WIDTH'(INITIAL_COUNT);
+      count <= initial_value.count;
     end
-    else if (busy) begin
+    else if (i_valid) begin
       count <= count - COUNT_WIDTH'(1);
     end
   end
 
   always_ff @(posedge i_clk) begin
     if (start) begin
-      result  <= get_initial_result(i_div_operation, i_rs1_value);
+      result  <= initial_value.result;
     end
     else if (calcuration) begin
       result  <= do_division(i_div_operation, i_rs2_value, result);
@@ -71,30 +77,112 @@ module rice_core_div
     logic [XLEN-1:0]        dividend,
     logic [XLEN-1:0]        divisor
   );
-    return is_signed(div_operation) && (dividend[XLEN-1] != divisor[XLEN-1]);
+    return
+      is_signed(div_operation) &&
+      (dividend[XLEN-1] != divisor[XLEN-1]);
+  endfunction
+
+  function automatic logic [DIVIDEND_WIDTH-1:0] extend_sign(
+    rice_core_div_operation div_operation,
+    logic [XLEN-1:0]        value
+  );
+    if (is_signed(div_operation)) begin
+      return {value[XLEN-1], value};
+    end
+    else begin
+      return DIVIDEND_WIDTH'(value);
+    end
+  endfunction
+
+  function automatic rice_core_div_initial_value get_initial_value(
+    rice_core_div_operation div_operation,
+    logic [XLEN-1:0]        rs1_value
+  );
+    logic                       sign_bit;
+    logic [XLEN-1:0]            match_sign;
+    rice_core_div_initial_value initial_value[XLEN];
+    logic [RESULT_WIDTH-1:0]    result;
+    logic [COUNT_WIDTH-1:0]     count;
+
+    sign_bit  = is_signed(div_operation) && rs1_value[XLEN-1];
+    for (int i = 0;i < XLEN;++i) begin
+      match_sign[i] =
+        (rs1_value[i] == sign_bit) &&
+        (((i + 1) < XLEN) || (rs1_value != VALUE_MIN));
+    end
+
+    for (int i = 0;i < XLEN;++i) begin
+      initial_value[XLEN-(i+1)].result  = {{XLEN{sign_bit}}, XLEN'(rs1_value << i)};
+      initial_value[XLEN-(i+1)].count   = COUNT_WIDTH'(INITIAL_COUNT - i);
+    end
+
+    for (int i = (XLEN - 1);i >= 1;--i) begin
+      if (!match_sign[i]) begin
+        return initial_value[i];
+      end
+    end
+
+    return initial_value[0];
+  endfunction
+
+  function automatic logic [RESULT_WIDTH-1:0] do_division(
+    rice_core_div_operation   div_operation,
+    logic [XLEN-1:0]          rs2_value,
+    logic [RESULT_WIDTH-1:0]  result
+  );
+    logic [DIVIDEND_WIDTH-1:0]  dividend;
+    logic [DIVIDEND_WIDTH-1:0]  a;
+    logic [DIVIDEND_WIDTH-1:0]  b;
+    logic [DIVIDEND_WIDTH-1:0]  c;
+
+    dividend  = result[RESULT_WIDTH-1-:DIVIDEND_WIDTH];
+    a         = dividend;
+    b         = extend_sign(div_operation, rs2_value);
+    if (modify_sign(div_operation, XLEN'(a), XLEN'(b))) begin
+      c = a + b;
+    end
+    else begin
+      c = a - b;
+    end
+
+    if (needs_recovery(dividend, c)) begin
+      return RESULT_WIDTH'({result, 1'(0)});
+    end
+    else begin
+      return RESULT_WIDTH'({c, result[RESULT_WIDTH-DIVIDEND_WIDTH-1:0], 1'(1)});
+    end
+  endfunction
+
+  function automatic logic needs_recovery(
+    logic [DIVIDEND_WIDTH-1:0]  dividend,
+    logic [DIVIDEND_WIDTH-1:0]  result
+  );
+    return
+      (result[DIVIDEND_WIDTH-1] != dividend[DIVIDEND_WIDTH-1]) &&
+      (result != DIVIDEND_WIDTH'(0));
   endfunction
 
   function automatic logic [XLEN-1:0] get_result(
     rice_core_div_operation   div_operation,
     logic [XLEN-1:0]          rs1_value,
     logic [XLEN-1:0]          rs2_value,
-    rice_core_division_result result
+    logic [RESULT_WIDTH-1:0]  result
   );
     logic div_zero;
     logic overflow;
+    logic mod_sign;
     logic div;
     logic divu;
     logic rem;
     logic remu;
-    logic mod_sign;
 
     div_zero  = rs2_value == XLEN'(0);
-    overflow  = is_signed(div_operation) && (rs1_value == {1'(1), {XLEN-1{1'(0)}}}) && (rs2_value == '1);
+    overflow  = is_overflow(div_operation, rs1_value, rs2_value);
+    mod_sign  = modify_sign(div_operation, rs1_value, rs2_value);
     div       = div_operation.div;
     divu      = div_operation.divu;
     rem       = div_operation.rem;
     remu      = div_operation.remu;
-    mod_sign  = modify_sign(div_operation, rs1_value, rs2_value);
     case ({div_zero, overflow, div, divu, rem, remu}) inside
       6'b1?1???,
       6'b1??1??:  return '1;
@@ -103,72 +191,18 @@ module rice_core_div
       6'b?11???:  return rs1_value;
       6'b?1??1?:  return '0;
       6'b00??1?,
-      6'b00???1:  return result.remainder[REMAINDER_WIDTH-1-:XLEN];
-      default:    return (mod_sign) ? -result.quotient : result.quotient;
+      6'b00???1:  return result[1*XLEN+:XLEN];
+      default:    return (mod_sign) ? -result[0*XLEN+:XLEN] : result[0*XLEN+:XLEN];
     endcase
   endfunction
 
-  function automatic rice_core_division_result get_initial_result(
-    rice_core_div_operation div_operation,
-    logic [XLEN-1:0]        rs1_value
+  function automatic logic is_overflow(
+    rice_core_div_operation     div_operation,
+    logic [XLEN-1:0]            rs1_value,
+    logic [XLEN-1:0]            rs2_value
   );
-    rice_core_division_result result;
-
-    result.quotient = XLEN'(0);
-    if (is_signed(div_operation)) begin
-      result.remainder  = {{XLEN{rs1_value[XLEN-1]}}, rs1_value};
-    end
-    else begin
-      result.remainder  = REMAINDER_WIDTH'(rs1_value);
-    end
-
-    return result;
+    return
+      is_signed(div_operation) &&
+      (rs1_value == VALUE_MIN) && (rs2_value == VALUE_MINUS_1);
   endfunction
-
-  function automatic rice_core_division_result do_division(
-    rice_core_div_operation   div_operation,
-    logic [XLEN-1:0]          rs2_value,
-    rice_core_division_result current_result
-  );
-    logic [REMAINDER_WIDTH-1:0] remainder;
-    logic [SUB_WIDTH-1:0]       a;
-    logic [SUB_WIDTH-1:0]       b;
-    logic [SUB_WIDTH-1:0]       c;
-    rice_core_division_result   result;
-
-    remainder = current_result.remainder;
-    a         = remainder[REMAINDER_WIDTH-1-:SUB_WIDTH];
-    if (is_signed(div_operation)) begin
-      b = {rs2_value[XLEN-1], rs2_value};
-    end
-    else begin
-      b = {1'(0), rs2_value};
-    end
-
-    if (modify_sign(div_operation, XLEN'(a), rs2_value)) begin
-      c = a + b;
-    end
-    else begin
-      c = a - b;
-    end
-
-    if ((c[SUB_WIDTH-1] == a[SUB_WIDTH-1]) || (c == SUB_WIDTH'(0))) begin
-      result.remainder  = REMAINDER_WIDTH'({c, remainder[REMAINDER_WIDTH-SUB_WIDTH-1:0], 1'(0)});
-      result.quotient   = XLEN'({current_result.quotient, 1'(1)});
-    end
-    else begin
-      result.remainder  = REMAINDER_WIDTH'({remainder, 1'(0)});
-      result.quotient   = XLEN'({current_result.quotient, 1'(0)});
-    end
-
-    return result;
-  endfunction
-
-  if (RICE_CORE_DEBUG) begin : g_debug
-    logic [SUB_WIDTH-1:0] a;
-
-    always_comb begin
-      a = result.remainder[REMAINDER_WIDTH-1-:SUB_WIDTH];
-    end
-  end
 endmodule
